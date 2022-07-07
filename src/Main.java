5de293b84dba524495873b23dd65c3c5880224d3
Ерошкин Мирослав <eroshkin_mir.ru@mail.ru>
2022-07-07 22:18:54 +0300

    Change

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int cost = 7499;
-        int x = 20;
+        int readout = 20;
         int mile;
-        if (cost < x) {
+        if (cost < readout) {
             mile = 0;
         } else {
-            mile = cost / x;
+            mile = cost / readout;
         }
         System.out.println("Начислится бесплатных миль: " + mile);
     }
